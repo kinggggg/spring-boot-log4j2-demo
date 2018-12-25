ee525401e7adb0d86fd8ce95738388642b97787d
zeek <liweibooooo@126.com>
2018-12-25 15:19:49 +0800

    修改SpringBoot使用log4j2，不使用默认的logback

---
+++ src/main/java/com/zeek/learn/springbootlogforjtwo/SpringbootlogforjtwoApplication.java
@@ -0,0 +1,14 @@
+package com.zeek.learn.springbootlogforjtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlogforjtwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootlogforjtwoApplication.class, args);
+	}
+
+}
+
